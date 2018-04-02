b86abd8461ad374763d87e601d597b2d2700267e
Pulppy <thocon7278@gmail.com>
2018-04-02 11:32:36 +0700

    Update Basic Product

---
+++ src/com/pulppy/user/actionTest/ProductDATA.java
@@ -0,0 +1,32 @@
+package com.pulppy.user.actionTest;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import com.pulppy.user.bean.ProductDTO;
+import com.pulppy.user.model.InteractiveProduct;
+
+public class ProductDATA {
+	
+	/* DTO Data Test */
+	public static ProductDTO setProductDTO(){
+		ProductDTO dto = new ProductDTO();
+		dto.setProductName("Galaxy Note 8");
+		dto.setProductImage("None");
+		dto.setProductCatelogy("Mobile");
+		dto.setProductStore("Samsung Center");
+		dto.setProductPriceDefault(22000000);
+		dto.setProductBrand("Samsung");
+		dto.setProductShortDecription("This is Samsung Note 8");
+		dto.setProductAmount(200);
+		dto.setProductEventStatus(true);
+		dto.setProductRate(5);
+		dto.setProductEventStatus(true);
+		dto.setProductPriceEvent(200001);
+		dto.setProductEventStart("12/02/2012");
+		dto.setProductEventEnd("13/02/2012");
+		dto.setProductID("4XGPDMGR23O4ZEQUJ");
+		
+		return dto;
+	}
+}
--- src/com/pulppy/user/model/InteractiveProduct.java
+++ src/com/pulppy/user/model/InteractiveProduct.java
@@ -3,6 +3,10 @@ package com.pulppy.user.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.pulppy.user.bean.ProductDTO;
 import com.pulppy.user.connection.MySQLConnUtils;
@@ -15,14 +19,15 @@ public class InteractiveProduct {
 	 * Describe : Insert Music 
 	 * Return: true
 	 */
-	public boolean createMusic(ProductDTO product) throws ClassNotFoundException, SQLException {
+	public boolean createProduct(ProductDTO product) throws ClassNotFoundException, SQLException {
 		boolean result = false;
 		result = insertProduct(product);
 		return result;
 	}
 	
 	/*
-	 * Function Name : insertAccount Describe : Check Duplicate ID and Insert
+	 * Function Name : insertAccount 
+	 * Describe : Check Duplicate ID and Insert
 	 * Return Value: String
 	 */
 	private boolean insertProduct(ProductDTO product)
@@ -63,21 +68,65 @@ public class InteractiveProduct {
 		return false;
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ProductDTO dto = new ProductDTO();
-		dto.setProductName("Testing");
-		dto.setProductImage("None");
-		dto.setProductCatelogy("None");
-		dto.setProductStore("None");
-		dto.setProductPriceDefault(100);
-		dto.setProductBrand("None");
-		dto.setProductShortDecription("none");
-		dto.setProductAmount(100);
-		dto.setProductEventStatus(true);
-		dto.setProductRate(5);
-		
-		InteractiveProduct a = new InteractiveProduct();
-		boolean check = a.insertProduct(dto);
-		System.out.println("Result " + check);
+	/*
+	 * Function Name : Update Product 
+	 * Describe : Update Production
+	 * Return Value: true
+	 */
+	public boolean updateProduct(ProductDTO product)
+			throws ClassNotFoundException, SQLException, ParseException {
+		if (product != null) {
+			// Query Duplicate ID
+			String sql = "Update pProduct set productName=?, productImage=?, productCatelogy=?,"
+					+ " productPriceDefault=?, productBrand=?, productShortDecription=?,"
+					+ " productAmount=?, productEventStatus=?, productEventStart=?,"
+					+ " productEventEnd=?,productPriceEvent=?"
+					+ " where id =?";
+			
+			
+			MySQLConnUtils sqlconn = new MySQLConnUtils();
+			Connection conn = sqlconn.getMySQLConnection();
+			PreparedStatement pstm = conn.prepareStatement(sql);
+			pstm.setString(1, product.getProductName());
+			pstm.setString(2, product.getProductImage());
+			pstm.setString(3, product.getProductCatelogy());
+			pstm.setInt(4, product.getProductPriceDefault());
+			pstm.setString(5, product.getProductBrand());
+			pstm.setString(6, product.getProductShortDecription());
+			pstm.setInt(7, product.getProductAmount());
+			
+			// Checking Event Status for Update Event
+			if(product.isProductEventStatus() == true ){
+				Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(product.getProductEventStart());  
+			    Date date2 = new SimpleDateFormat("dd/MM/yyyy").parse(product.getProductEventEnd()); 
+			    // Checking Date Event Valid
+			    if(date2.compareTo(date1) >= 0){			    	
+			    	pstm.setBoolean(8, product.isProductEventStatus());	
+					pstm.setString(9, product.getProductEventStart());
+					pstm.setString(10, product.getProductEventEnd());
+					// Checking Event price Limit
+					if(product.getProductPriceEvent() >= 10000){
+						pstm.setInt(11, product.getProductPriceEvent());
+					}else{
+						System.out.println("Gia Khong Dat Yeu Cau");
+						return false;
+					}
+			    }else{
+			    	System.out.println("Date input False");
+			    	return false;
+			    }
+				
+			}
+			pstm.setString(12,product.getProductID());
+			
+			pstm.executeUpdate();
+			conn.close();
+			pstm.close();
+			return true;
+			
+		}
+		System.out.println("Choose Product");
+		return false;
 	}
+	
 }
